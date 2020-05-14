f3b35c6c9e1256ad704ab57f8ceb189b297ae47c
Nick Goldstein <nickagoldstein@gmail.com>
2020-05-14 10:49:35 -0400

    Initial commit

---
+++ cdotp.cwp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cdotp.cwp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
